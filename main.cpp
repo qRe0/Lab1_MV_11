36e0572814b83281760f7b11396b6fbbb1f18ec4
qRe <melsenmalina@gmail.com>
2023-10-03 21:00:35 +0300

    Refactored version

--- main.cpp
+++ main.cpp
@@ -6,95 +6,24 @@
 
 using namespace std;
 
-const int N = 3;
-
-void printMatrix(double** matrix, int N);
-void printVector(double* vector, int N);
-
-double* solveWithGauss(double** A, double* b, int N);
-double* solveWithCholesky(double** A, double* b, int N);
-double computeDeterminant(double** A, int N);
-
-int main() {
-
-    srand(time(NULL));
-
-    double** A = new double*[N];
-    for(int i = 0; i < N; i++) {
-        A[i] = new double[N];
-    }
-
-    double* b = new double[N];
-
-    // Заполнение матрицы
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            if(i == j) {
-                A[i][j] = 5 * sqrt(i+1);
-            } else {
-                A[i][j] = sqrt((i+1)*(j+1));
-            }
-        }
-    }
-
-    // Заполнение вектора случайными значениями
-    for(int i = 0; i < N; i++) {
-        b[i] = (double)rand() / RAND_MAX;
-    }
-
-    cout << "Matrix:" << endl;
-    printMatrix(A, N);
-
-    cout << "Vector b:" << endl;
-    printVector(b, N);
-
-    // Решение системы методом Гаусса
-    double* xGauss = solveWithGauss(A, b, N);
-
-    // Вычисление определителя
-    double determinantA = computeDeterminant(A, N);
-
-    cout << "Solve with Gauss" << endl;
-    printVector(xGauss, N);
-
-    cout << "Det A: " << determinantA << endl;
-
-    // Решение системы методом Холецкого
-    double* xCholesky = solveWithCholesky(A, b, N);
-
-    cout << "Solve with Cholesky:" << endl;
-    printVector(xCholesky, N);
-
-    // Освобождение памяти
-    for(int i = 0; i < N; i++) {
-        delete[] A[i];
-    }
-    delete[] A;
-    delete[] b;
-    delete[] xGauss;
-    delete[] xCholesky;
-
-    return 0;
-}
-
 // Метод Гаусса
-double* solveWithGauss(double** A, double* b, int N) {
+double *solveWithGauss(double **A, double *b, int N) {
 
-    double* x = new double[N];
+    auto *x = new double[N];
 
     // Прямой ход
-    for(int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
 
         // Выбор главного элемента
         int maxRow = i;
-        for(int k = i+1; k < N; k++) {
-            if(abs(A[k][i]) > abs(A[maxRow][i]))
+        for (int k = i + 1; k < N; k++) {
+            if (abs(A[k][i]) > abs(A[maxRow][i]))
                 maxRow = k;
         }
 
         // Перестановка строк
-        if(maxRow != i) {
-            double* temp = A[i];
+        if (maxRow != i) {
+            double *temp = A[i];
             A[i] = A[maxRow];
             A[maxRow] = temp;
 
@@ -104,19 +33,19 @@ double* solveWithGauss(double** A, double* b, int N) {
         }
 
         // Обнуление элементов ниже главного
-        for(int k = i+1; k < N; k++) {
+        for (int k = i + 1; k < N; k++) {
             double factor = A[k][i] / A[i][i];
             b[k] -= factor * b[i];
-            for(int j = i; j < N; j++) {
+            for (int j = i; j < N; j++) {
                 A[k][j] -= factor * A[i][j];
             }
         }
     }
 
     // Обратный ход
-    for(int i = N-1; i >= 0; i--) {
+    for (int i = N - 1; i >= 0; i--) {
         x[i] = b[i] / A[i][i];
-        for(int j = i+1; j < N; j++) {
+        for (int j = i + 1; j < N; j++) {
             x[i] -= A[i][j] * x[j] / A[i][i];
         }
     }
@@ -125,11 +54,11 @@ double* solveWithGauss(double** A, double* b, int N) {
 }
 
 // Вычисление определителя
-double computeDeterminant(double** A, int N) {
+double computeDeterminant(double **A, int N) {
 
     double determinant = 1.0;
 
-    for(int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
         determinant *= A[i][i];
     }
 
@@ -137,28 +66,28 @@ double computeDeterminant(double** A, int N) {
 }
 
 // Метод Холецкого
-double* solveWithCholesky(double** A, double* b, int N) {
+double *solveWithCholesky(double **A, const double *b, int N) {
 
     // Разложение на нижнюю L и верхнюю U матрицы
-    double** L = new double*[N];
-    double** U = new double*[N];
-    for(int i = 0; i < N; i++) {
+    auto **L = new double *[N];
+    auto **U = new double *[N];
+    for (int i = 0; i < N; i++) {
         L[i] = new double[N];
         U[i] = new double[N];
     }
 
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j <= i; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j <= i; j++) {
             double s = 0;
-            for(int k = 0; k < j; k++) {
+            for (int k = 0; k < j; k++) {
                 s += L[i][k] * U[k][j];
             }
             L[i][j] = A[i][j] - s;
         }
 
-        for(int j = i; j < N; j++) {
+        for (int j = i; j < N; j++) {
             double s = 0;
-            for(int k = 0; k < i; k++) {
+            for (int k = 0; k < i; k++) {
                 s += L[i][k] * U[k][j];
             }
             U[i][j] = (A[i][j] - s) / L[i][i];
@@ -166,31 +95,31 @@ double* solveWithCholesky(double** A, double* b, int N) {
     }
 
     // Решение Ly = b
-    double* y = new double[N];
+    auto *y = new double[N];
     y[0] = b[0] / L[0][0];
 
-    for(int i = 1; i < N; i++) {
+    for (int i = 1; i < N; i++) {
         double s = 0;
-        for(int j = 0; j < i; j++) {
+        for (int j = 0; j < i; j++) {
             s += L[i][j] * y[j];
         }
         y[i] = (b[i] - s) / L[i][i];
     }
 
     // Решение Ux = y
-    double* x = new double[N];
-    x[N-1] = y[N-1] / U[N-1][N-1];
+    auto *x = new double[N];
+    x[N - 1] = y[N - 1] / U[N - 1][N - 1];
 
-    for(int i = N-2; i >= 0; i--) {
+    for (int i = N - 2; i >= 0; i--) {
         double s = 0;
-        for(int j = i+1; j < N; j++) {
+        for (int j = i + 1; j < N; j++) {
             s += U[i][j] * x[j];
         }
         x[i] = (y[i] - s) / U[i][i];
     }
 
     // Освобождение памяти
-    for(int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
         delete[] L[i];
         delete[] U[i];
     }
@@ -202,12 +131,12 @@ double* solveWithCholesky(double** A, double* b, int N) {
 }
 
 // Вывод матрицы
-void printMatrix(double** matrix, int N) {
+void printMatrix(double **matrix, int N) {
 
     cout << fixed << setprecision(4);
 
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
@@ -215,11 +144,80 @@ void printMatrix(double** matrix, int N) {
 }
 
 // Вывод вектора
-void printVector(double* vector, int N) {
+void printVector(double *vector, int N) {
 
     cout << fixed << setprecision(4);
 
-    for(int i = 0; i < N; i++) {
-        cout << vector[i] << endl;
+    for (int i = 0; i < N; i++) {
+        cout << "|" << vector[i] << "|" << endl;
     }
+}
+
+int main() {
+
+    const int N = 2;
+
+    srand(time(nullptr));
+
+    auto **A = new double *[N];
+    for (int i = 0; i < N; i++) {
+        A[i] = new double[N];
+    }
+
+    auto *b = new double[N];
+
+    // Заполнение матрицы
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == j) {
+                A[i][j] = 5 * sqrt(i + 1);
+            } else {
+                A[i][j] = sqrt((i + 1) * (j + 1));
+            }
+        }
+    }
+
+    // Заполнение вектора случайными значениями
+    for (int i = 0; i < N; i++) {
+        b[i] = (double) rand() / RAND_MAX;
+    }
+
+    cout << endl;
+    cout << "Matrix:" << endl;
+    printMatrix(A, N);
+
+    cout << endl;
+    cout << "Vector b:" << endl;
+    printVector(b, N);
+
+    // Решение системы методом Гаусса
+    double *xGauss = solveWithGauss(A, b, N);
+
+    // Вычисление определителя
+    double determinantA = computeDeterminant(A, N);
+
+    cout << endl;
+    cout << "Solve with Gauss" << endl;
+    printVector(xGauss, N);
+
+    cout << endl;
+    cout << "Det A: " << determinantA << endl;
+
+    // Решение системы методом Холецкого
+    double *xCholesky = solveWithCholesky(A, b, N);
+
+    cout << endl;
+    cout << "Solve with Cholesky:" << endl;
+    printVector(xCholesky, N);
+
+    // Освобождение памяти
+    for (int i = 0; i < N; i++) {
+        delete[] A[i];
+    }
+    delete[] A;
+    delete[] b;
+    delete[] xGauss;
+    delete[] xCholesky;
+
+    return 0;
 }
